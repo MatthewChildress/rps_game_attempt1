8e9175f2075758271a9985cc65b77720dbf37cfc
Matthew Childress <matt.t.childress@gmail.com>
2022-02-18 09:39:46 -0500

    created HTML file. wrote intructions in script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+//need computerPlay that will randomly return rock,paper,scissors
+//playRound does single round of rock,paper,scissors
+//need strings to RETURN results declaring winner
+// parameters playerSelection & computerSelection are each choice
+
+
+
+function playRound(playerSelection, computerSelection) {
+    //info for round
+}
+
+const playerSelection = "rock";
+// make playerSelection similar to computerSelection
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+// has playRound and loops in it
+
+}
+
+// loop shown in instructions
+for (let i = 0; i < 5; i++) {
+    //figure out where
+}
+
+//use prompt to get player info
